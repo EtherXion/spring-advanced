962737da42cd2ba109de0daf5e77c43d6e11faca
EtherXion <etel0103@gmail.com>
2024-09-12 14:52:19 +0900

    Level 2 - 9 Aop 파일 생성

---
+++ src/main/java/org/example/expert/aop/AccessLogAop.java
@@ -0,0 +1,15 @@
+package org.example.expert.aop;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j(topic = "AccessLogAop")
+@Aspect
+@Component
+public class AccessLogAop {
+
+
+
+}
